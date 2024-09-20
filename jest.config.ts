ad9c9ff61a4b92a84b055124bb132b09d110f8d5
gustavo-nt <gustavont.dev@gmail.com>
2024-09-19 21:49:53 -0300

    feat: add some paths to ignore on coverage test

--- jest.config.ts
+++ jest.config.ts
@@ -28,9 +28,14 @@ const config: Config = {
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+  coveragePathIgnorePatterns: [
+    "src/utils",
+    "src/context/",
+    "src/reducers/",
+    "src/services/",
+    "src/tests/",
+    "src/utils/",
+  ],
 
   // Indicates which provider should be used to instrument code for coverage
   // coverageProvider: "babel",
